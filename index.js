08832c16919a717313d6bb3b7efe9912041f11ea
ex-lihonghan <ex-lihonghan@dashuf.com>
2021-05-25 17:00:33 +0800

    瞎几把计算

---
+++ index.js
@@ -0,0 +1,58 @@
+const XLSX = require('xlsx');
+
+const workBook = XLSX.readFile('./TransactionHistory-20210525.xlsx');
+
+const sheet = workBook.SheetNames;
+
+const workSheet = workBook.Sheets[sheet[0]];
+
+let list = XLSX.utils.sheet_to_json(workSheet);
+
+let json = JSON.stringify(list);
+
+const exChange = {
+  '时间': 'time',
+  '交易类型': 'tradeType',
+  '交易对': 'tradePair',
+  '方向': 'tradeDirection',
+  '价格': 'price',
+  '数量': 'num',
+  '成交额': 'trandeCost',
+  '手续费': 'poundage'
+}
+
+const keys = Object.keys(exChange);
+keys.forEach(v => {
+  let reg = new RegExp(v, 'g');
+  json = json.replace(reg, exChange[v]);
+});
+
+list = JSON.parse(json);
+
+list.sort((a, b) => {
+  return a.time - b.time;
+});
+
+
+// console.log(list);
+
+let singleTrande = list[0].trandeCost / (list[0].num * (1 - 0.002));
+
+let totalCost = 0;
+let totalCoin = 0;
+
+list.forEach(v => {
+  if (v.tradeDirection === '买入') {
+    totalCost += v.trandeCost;
+    totalCoin += (v.num * (1 - 0.002))
+  }
+  if (v.tradeDirection === '卖出') {
+    totalCost -= v.trandeCost;
+    totalCost += parseFloat(v.poundage);
+    totalCoin -= v.num
+  }
+});
+
+console.log(totalCoin);
+
+console.log(totalCost / totalCoin);
